4838a1b14432d51faf600fd6a4db18fda6c234c2
Bizzi <106081382+MiguelBizzi@users.noreply.github.com>
2022-08-26 16:45:09 -0300

    Primeiro Commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+
+// pegando mario 
+const mario = document.querySelector('.mario');
+
+// pegando pipe
+const pipe = document.querySelector('.pipe');
+
+//adicionando a classe do pulo
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+
+        mario.classList.remove('jump');
+
+    }, 500);
+}
+
+document.addEventListener('keydown', jump);
+
+
+// loop para ver se perdemos 
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    //o + na frente transforma a string em numero || mesma funcao do number() ou parseInt
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
+
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+        
+
+        mario.src = 'imagens/game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
+
+        clearInterval(loop);
+    }
+
+}, 10);
